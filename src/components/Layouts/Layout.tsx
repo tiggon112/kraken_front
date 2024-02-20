303dec21448fcd4bb28d92afdeff6ec7232b2158
tiggon112 <tiggon112@gmail.com>
2024-02-20 07:34:47 -0500

    build layout

---
+++ src/components/Layouts/Layout.tsx
@@ -0,0 +1,25 @@
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <div className="flex w-full justify-center bg-[#f8f1f0]">
+      <div className="fixed top-0 z-50 box-border border-b border-solid border-custom-grey">
+        <Header />
+        <Sidebar />
+      </div>
+      <div className="flex w-full justify-center">
+        <div
+          id="content"
+          className="col-span-8 mt-[3.5rem] flex h-[calc(100vh-3.75rem)] w-full md:pl-42 xl:pl-[calc((100vw-1248px)/2+16.25rem+2rem+0rem)]"
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
