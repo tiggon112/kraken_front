504bbdf3dc6d2e6514761fac626ab94bbbaedbde
tiggon112 <tiggon112@gmail.com>
2024-02-20 07:42:43 -0500

    fixed svg type error

--- src/components/SVG/Percent.tsx
+++ src/components/SVG/Percent.tsx
@@ -17,13 +17,13 @@ const Percent = () => {
       <path
         d="M17 20C18.6569 20 20 18.6569 20 17C20 15.3431 18.6569 14 17 14C15.3431 14 14 15.3431 14 17C14 18.6569 15.3431 20 17 20Z"
         strokeLinecap="round"
-        stroke-linejoin="round"
+        strokeLinejoin="round"
         className="fill-transparent"
       ></path>
       <path
         d="M7 10C8.65685 10 10 8.65685 10 7C10 5.34315 8.65685 4 7 4C5.34315 4 4 5.34315 4 7C4 8.65685 5.34315 10 7 10Z"
         strokeLinecap="round"
-        stroke-linejoin="round"
+        strokeLinejoin="round"
         className="fill-transparent"
       ></path>
     </svg>
